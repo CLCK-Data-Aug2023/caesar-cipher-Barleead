b8ef03b6af91c1e063598dd0a8000a0d003f3142
Barbi Adkins <badkins0043@gmail.com>
2023-10-31 00:26:38 -0400

    program done

--- cipher.py
+++ cipher.py
@@ -1,1 +1,19 @@
-# add your code here
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+shift = int(input("Number of spaces to shift: "))
+
+phrase = input("Please enter a word or phrase to encrypt: ")
+
+phrase = phrase.lower()
+
+encrypted_phrase = ""
+
+for char in phrase:
+    if char in alphabet:
+        index = alphabet.find(char)
+        index = (index + shift) % 26
+        char = alphabet[index]
+    encrypted_phrase += char
+
+
+print(encrypted_phrase)
